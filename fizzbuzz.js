9b7ccc9de75adf9a70837d18b25ce7eb49519fb5
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-01-26 12:14:26 -0500

    Forgot to add comments to my code

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,8 @@
+// Code written by Jared Smith
+// Fizzbuzz problem
+// prints out numbers from 1 to 100 and then fizz for divisble by 3
+// and buzz for divisble by 5
+// Last work on the problem was on Friday
 for (var number = 1; number <= 100; number++) {
   if (number % 3 == 0 && number % 5 == 0)
     console.log("fizzbuzz");
